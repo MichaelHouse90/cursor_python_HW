c4e4e5f55074df55ab5c18ac32f9c54dba1a1a09
Michael <michaelhouse90@gmail.com>
2018-09-26 21:34:11 +0300

    Initial commit

---
+++ Homework_PIP_Virtualenv_Virtualenvwrapper/task5.py
@@ -0,0 +1,17 @@
+# Write a function that prints numbers from 1 to 30. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.
+
+def funcFizzBuzz():
+
+  list = []
+  for i in range(1, 31):
+    if i % 3 == 0 and i % 5 == 0:
+      list.append("FizzBuzz")
+    elif i % 3 == 0:
+      list.append("Fizz")
+    elif i % 5 == 0:
+      list.append("Buzz")
+    else:
+      list.append(i)
+  print(list)
+
+funcFizzBuzz()

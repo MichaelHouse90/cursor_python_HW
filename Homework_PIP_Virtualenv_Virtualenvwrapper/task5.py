41ee1e5fb833e63bf688a3ce17d104a562a6cc2b
Michael <michaelhouse90@gmail.com>
2018-10-16 21:54:12 +0300

    refactoring

--- Homework_PIP_Virtualenv_Virtualenvwrapper/task5.py
+++ Homework_PIP_Virtualenv_Virtualenvwrapper/task5.py
@@ -1,17 +1,15 @@
-# Write a function that prints numbers from 1 to 30. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.
+def func_fizz_buzz():
+    list = []
+    for i in range(1, 31):
+        if i % 3 == 0 and i % 5 == 0:
+            list.append("FizzBuzz")
+        elif i % 3 == 0:
+            list.append("Fizz")
+        elif i % 5 == 0:
+            list.append("Buzz")
+        else:
+            list.append(i)
+    print(list)
 
-def funcFizzBuzz():
 
-  list = []
-  for i in range(1, 31):
-    if i % 3 == 0 and i % 5 == 0:
-      list.append("FizzBuzz")
-    elif i % 3 == 0:
-      list.append("Fizz")
-    elif i % 5 == 0:
-      list.append("Buzz")
-    else:
-      list.append(i)
-  print(list)
-
-funcFizzBuzz()
+func_fizz_buzz()

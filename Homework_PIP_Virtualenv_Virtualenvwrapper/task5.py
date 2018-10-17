75c47cb50d2bcbfb80cc090eb049cd301b156d12
Michael <michaelhouse90@gmail.com>
2018-10-17 20:15:51 +0300

    refactoring after comments of support

--- Homework_PIP_Virtualenv_Virtualenvwrapper/task5.py
+++ Homework_PIP_Virtualenv_Virtualenvwrapper/task5.py
@@ -1,15 +1,15 @@
 def func_fizz_buzz():
-    list = []
+    fizz_buzz_list = []
     for i in range(1, 31):
+        default_value = str(i)
         if i % 3 == 0 and i % 5 == 0:
-            list.append("FizzBuzz")
+            default_value = 'FizzBuzz'
         elif i % 3 == 0:
-            list.append("Fizz")
+            default_value = 'Fizz'
         elif i % 5 == 0:
-            list.append("Buzz")
-        else:
-            list.append(i)
-    print(list)
+            default_value = 'Buzz'
+        fizz_buzz_list.append(default_value)
+    return fizz_buzz_list
 
 
-func_fizz_buzz()
+print(func_fizz_buzz())
